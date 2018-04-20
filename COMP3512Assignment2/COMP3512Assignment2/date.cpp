9eb2ecb2e037f0717a32de49eacad9a56aa1425d
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-04-20 12:03:07 -0700

    Finished commenting date.cpp

--- COMP3512Assignment2/COMP3512Assignment2/date.cpp
+++ COMP3512Assignment2/COMP3512Assignment2/date.cpp
@@ -9,6 +9,12 @@
 const int Date::days_in_regular_year[] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 const int Date::days_in_leap_year[] = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+/*
+Desc: Three parameter date constructor 
+Pre: Nothing
+Post: Creates a date object 
+Return: Nothing
+*/
 Date::Date(int year, int month, int day)
 	: year{ year }, month{ month }, day{ day } {
 	if (!is_valid_date(year, month, day)) {
@@ -16,6 +22,12 @@ Date::Date(int year, int month, int day)
 	}
 }
 
+/*
+Desc: Overloads the ++ operator for the date class
+Pre: A date object
+Post: Incremements the day in a dynamic fashion
+Return: A reference to the current date 
+*/
 Date&
 Date::operator++()
 {
@@ -30,6 +42,12 @@ Date::operator++()
 	return *this;
 }
 
+/*
+Desc:Overloads the prefix ++operator 
+Pre: A date object
+Post: Increments the date via prefix 
+Return: A copy of this date reference 
+*/
 Date
 Date::operator++(int)
 {
@@ -38,6 +56,12 @@ Date::operator++(int)
 	return copy;
 }
 
+/*
+Desc: Overloads the -- operator for the date object
+Pre: A date object
+Post: Dynamically decrements the date object
+Return: A reference to this date object
+*/
 Date&
 Date::operator--()
 {
@@ -52,6 +76,12 @@ Date::operator--()
 	return *this;
 }
 
+/*
+Desc: Overloads the prefix -- operator for the date object
+Pre: A date object
+Post: Dynamically prefix decrements the date object
+Return: A reference to a copy of this date object 
+*/
 Date
 Date::operator--(int)
 {
@@ -60,6 +90,12 @@ Date::operator--(int)
 	return copy;
 }
 
+/*
+Desc: Sets the date values for year month day
+Pre: A date objecft
+Post: Sets the values
+Return: A copy of this date object 
+*/
 Date
 Date::set_date(int year, int month, int day)
 {
@@ -73,6 +109,12 @@ Date::set_date(int year, int month, int day)
 	return copy;
 }
 
+/*
+Desc: Determines if this year is a leap year
+Pre: A date object and a year
+Post: A boolean representing the answer
+Return: A bool 
+*/
 bool
 Date::is_leap_year(int year)
 {
@@ -89,6 +131,11 @@ Date::is_leap_year(int year)
 }
 
 
+/*
+Desc: Determines if this date is value
+Pre: A date object a year, month, day
+Post: A boolean representing the answer
+*/
 bool
 Date::is_valid_date(int year, int month, int day)
 {
@@ -96,6 +143,12 @@ Date::is_valid_date(int year, int month, int day)
 	return 1 <= month && month <= 12 && 1 <= day && day <= days[month];
 }
 
+/*
+Desc: Makes all the date info correspond to today
+Pre: A date object
+Post: All the date variables change apppropriately
+Return: A date object
+*/
 Date
 Date::today()
 {
@@ -106,6 +159,12 @@ Date::today()
 		timeinfo->tm_mday);
 }
 
+/*
+Desc: Overloads the << operator for the date object
+Pre: A date object and an ostream
+Post: Returns the os with the date info formated nicely
+Return: The ostream os
+*/
 std::ostream&
 operator<<(std::ostream& os, const Date& d)
 {
@@ -117,6 +176,12 @@ operator<<(std::ostream& os, const Date& d)
 	return os;
 }
 
+/*
+Desc: Overloads the >> operator for the date object
+Pre: A date object and an istream
+Post: Take the istream and inserts it into a date object properly
+Return: The istream reference 
+*/
 std::istream&
 operator>>(std::istream& is, Date& date)
 {
@@ -139,6 +204,12 @@ operator>>(std::istream& is, Date& date)
 	return is;
 }
 
+/*
+Desc: Checks the equality of the date on both sides
+Pre: Two date objects
+Post: Returns a boolean representing the answer
+Return: A boolean
+*/
 bool operator==(const Date& lhs, const Date& rhs)
 {
 	return lhs.year == rhs.year
@@ -146,11 +217,23 @@ bool operator==(const Date& lhs, const Date& rhs)
 		&& lhs.day == rhs.day;
 }
 
+/*
+Desc: Checks the inequality of the date on both sides
+Pre: Two date objects
+Post: Returns a boolean representing the answer
+Return: A boolean
+*/
 bool operator!=(const Date& lhs, const Date& rhs)
 {
 	return !operator==(lhs, rhs);
 }
 
+/*
+Desc: Checks if the rhs is greater than the lhs
+Pre: Two date objects
+Post: Returns a boolean representing the answer
+Return: A boolean
+*/
 bool operator<(const Date& lhs, const Date& rhs)
 {
 	if (lhs.year != rhs.year) {
@@ -165,16 +248,34 @@ bool operator<(const Date& lhs, const Date& rhs)
 	return false;
 }
 
+/*
+Desc: Checks if the lhs is greater than the rhs
+Pre: Two date objects
+Post: Returns a boolean representing the answer
+Return: A boolean
+*/
 bool operator>(const Date& lhs, const Date& rhs)
 {
 	return operator<(rhs, lhs);
 }
 
+/*
+Desc: Checks if the rhs is greater than or equal to the lhs
+Pre: Two date objects
+Post: Returns a boolean representing the answer
+Return: A boolean
+*/
 bool operator>=(const Date& lhs, const Date& rhs)
 {
 	return !operator<(lhs, rhs);
 }
 
+/*
+Desc: Checks if the rhs is greater than or equal to the lhs
+Pre: two date objects 
+Post: Returns a boolean representing the answer
+Return: A boolean 
+*/
 bool operator<=(const Date& lhs, const Date& rhs)
 {
 	return !operator>(lhs, rhs);

38cb7e5df87d4198c48e0cd223f86c6755dce4c0
Alex Abdulla <alexander.amadeus.abdulla@gmail.com>
2018-04-20 12:25:21 -0700

    Last commented file, the controller. <3

--- COMP3512Assignment2/COMP3512Assignment2/controller.cpp
+++ COMP3512Assignment2/COMP3512Assignment2/controller.cpp
@@ -7,9 +7,21 @@
 #include "database.hpp"
 #include "date.hpp"
 
+/*
+Desc: The default constructor fro the controller 
+Pre: Nothing
+Post: A default controller is created
+Return: Nothing
+*/
 Controller::Controller() {
 }
 
+/*
+Desc: Prints the controller's menu
+Pre: A controller
+Post: A printed menu
+Return: Nothing
+*/
 void Controller::print_menu() {
 	std::cout << "1. Add new patient" << std::endl;
 	std::cout << "2. Get next patient" << std::endl;
@@ -21,6 +33,12 @@ void Controller::print_menu() {
 	std::cout << "*. Clear screen" << std::endl;
 }
 
+/*
+Desc: Prints a status menu
+Pre: A controller
+Post: A printed status menu
+Return: Nothing
+*/
 void Controller::print_status_menu() {
 	std::cout << "Please, select patients status:" << std::endl;
 	std::cout << "1. Critical and life-threatening, requires immediate care" << std::endl;
@@ -31,6 +49,12 @@ void Controller::print_status_menu() {
 	std::cout << "6. Not a priority" << std::endl;
 }
 
+/*
+Desc: Determines whether the healthcare input is valid
+Pre: A controller and an input 
+Post: A boolean representing whether it is valid or not
+Return: A boolean value 
+*/
 bool Controller::is_valid_healthcare_input(std::string f) {
 	if (f.length() != 8) {
 		return false;
@@ -49,6 +73,12 @@ bool Controller::is_valid_healthcare_input(std::string f) {
 	return true;
 }
 
+/*
+Desc: Determines whether the time input is valid
+Pre: A controller and an input
+Post: A boolean representing whether it is valid or not
+Return: A boolean value
+*/
 bool Controller::is_valid_time(std::string f) {
 	if (f.length() != 5) {
 		return false;
@@ -67,6 +97,12 @@ bool Controller::is_valid_time(std::string f) {
 	return true;
 }
 
+/*
+Desc:Prompts the user for another healthcare number
+Pre: A controller
+Post: Prompt is displayed and content is returned 
+Return: Content
+*/
 std::string Controller::prompt_name(std::string f, bool req = false) {
 	std::cin.clear();
 	std::string content;
@@ -83,6 +119,12 @@ std::string Controller::prompt_name(std::string f, bool req = false) {
 	return content;
 }
 
+/*
+Desc: The input is cleared and ignored and a prompt is generated 
+Pre: A prompt string
+Post: A prompt is generated
+Return: Line 
+*/
 std::string Controller::prompt_sympt(std::string f) {
 	std::cin.clear();
 	std::cin.ignore();
@@ -92,6 +134,12 @@ std::string Controller::prompt_sympt(std::string f) {
 	return line;
 }
 
+/*
+Desc: Prompts for a number
+Pre: A controller
+Post: A number prompt is created
+Return: An int n_out
+*/
 int Controller::prompt_num(std::string f) {
 	std::string n;
 	std::cout << f;
@@ -100,6 +148,12 @@ int Controller::prompt_num(std::string f) {
 	return n_out;
 }
 
+/*
+Desc: Prompts for date of birth 
+Pre: A prompt string f and controller
+Post: Prompts are generated and displayed
+Return: A date d with the prompts 
+*/
 Date Controller::prompt_dob(std::string f) {
 	Date d;
 	int year = prompt_num("Year of birth: ");
@@ -115,6 +169,12 @@ Date Controller::prompt_dob(std::string f) {
 	return d;
 }
 
+/*
+Desc: A prompt time is generated 
+Pre: A controller and prompt string f
+Post: A prompt time is generated
+Return: Time t 
+*/
 time Controller::prompt_time(std::string f) {
 	time t;
 	int conv1, conv2;
@@ -132,6 +192,12 @@ time Controller::prompt_time(std::string f) {
 	return t;
 }
 
+/*
+Desc: The patient is compiled and values are set
+Pre: A patient and a controller 
+Post: The patient info is compiled
+Return: The freshly compiled patient p 
+*/
 Patient Controller::compile_patient() {
 	Patient p;
 	p.set_last(prompt_name("Last name: "));
@@ -147,6 +213,12 @@ Patient Controller::compile_patient() {
 	return p;
 }
 
+/*
+Desc: Changes the patient 
+Pre: A patient and a controller
+Post: The patient is changed
+Return: Nothing
+*/
 void Controller::change_patient() {
 	std::string num;
 	std::cout << "Enter the patient to change: " << std::endl;
@@ -163,6 +235,12 @@ void Controller::change_patient() {
 	}
 }
 
+/*
+Desc: Runs the controller menu
+Pre: A controller 
+Post: A menu actually run as a program
+Return: Nothing 
+*/
 void Controller::run_menu() {
 	print_menu();
 	char u_input = 'p';
